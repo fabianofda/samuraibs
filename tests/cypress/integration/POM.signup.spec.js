01a5283830c705a5041609c78a132a53a946c6aa
Fabiano Dias <fabianofda@gmail.comm>
2022-03-11 21:45:23 -0300

    'validacao mensagens de erro'

--- tests/cypress/integration/POM.signup.spec.js
+++ tests/cypress/integration/POM.signup.spec.js
@@ -92,5 +92,23 @@ describe('Cadastro - [POM]', function () {
 
     })
 
-    
+    context('quando não preencho nenhum dos campos', function () {
+        const alertMessages = [
+            'Nome é obrigatório',
+            'E-mail é obrigatório',
+            'Senha é obrigatória'
+        ]
+
+        before(function () {
+            signupPage.go()
+            signupPage.submit()
+        })
+
+        alertMessages.forEach(function (alert) {
+            it('deve exibir ' + alert.toLowerCase(), function () {
+                signupPage.alertHaveText(alert)
+            })
+        })
+
+    })
 })
