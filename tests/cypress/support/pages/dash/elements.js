0d170ef442e3cb5c1281a535f8badf61c885370f
Fabiano Dias <fabianofda@gmail.comm>
2022-04-01 18:30:12 -0300

    "finalizando o modulo de api"

--- tests/cypress/support/pages/dash/elements.js
+++ tests/cypress/support/pages/dash/elements.js
@@ -1,3 +1,8 @@
 exports.el = {
 
+    calendar: '.DayPicker',
+    boxDay: '.DayPicker-Day--available',
+    boxName: 'div',
+    boxHour: 'span[class=appointment]'
+
 }
--- tests/cypress/support/pages/dash/index.js
+++ tests/cypress/support/pages/dash/index.js
@@ -9,23 +9,23 @@ class DashPage {
 
     calendarShoulBeVisible() {
 
-        cy.get('.DayPicker', { timeout: 10000 })
+        cy.get(el.calendar, { timeout: 10000 })
             .should('be.visible')
     }
 
     selectDay(day) {
         const target = new RegExp('^' + day + '$', 'g')
 
-        cy.contains('.DayPicker-Day--available', target)
+        cy.contains(el.boxDay, target)
             .click({ force: true })
     }
 
     appointmentShoulBe(customer, hour) {
 
-        cy.contains('div', customer.name, { timeout: 10000 })
+        cy.contains(el.boxName, customer.name, { timeout: 10000 })
             .should('be.visible')
             .parent()
-            .contains('span[class=appointment]', hour)
+            .contains(el.boxHour, hour)
             .should('be.visible')
     }
 }
