04c5ca81b013c4fb7b59acfeb32387717db3ebdd
Fabiano Dias <fabianofda@gmail.comm>
2022-04-01 18:03:11 -0300

    "atualizando o appointments"

--- tests/cypress/integration/dash.spec.js
+++ tests/cypress/integration/dash.spec.js
@@ -1,4 +1,5 @@
-
+import loginPage from '../support/pages/login'
+import dashPage from '../support/pages/dash'
 
 describe('dashboard', function () {
 
@@ -15,58 +16,31 @@ describe('dashboard', function () {
                 email: 'ramon@televisa.com',
                 password: 'pwd123',
                 is_provider: true
-            }
+            },
+            appointmentHour: '17:00'
         }
 
         before(function () {
+
             cy.postUser(data.provider)
             cy.postUser(data.customer)
-
             cy.apiLogin(data.customer)
-           // cy.log(Cypress.env('apiToken'))
-
+            // cy.log(Cypress.env('apiToken'))
             cy.setProviderId(data.provider.email)
+            cy.createAppointment(data.appointmentHour)
         })
 
         it('o mesmo deve ser exibido no dashboard', function () {
-            cy.log(Cypress.env('providerId'))
+
+            loginPage.go()
+            loginPage.form(data.provider)
+            loginPage.submit()
+
+            dashPage.calendarShoulBeVisible()
+            const day = Cypress.env('appointmentDay')
+            dashPage.selectDay(day)
+            dashPage.appointmentShoulBe(data.customer, data.appointmentHour)
+
         })
-    })
-})
-
-Cypress.Commands.add('setProviderId', function (providerEmail) {
-    cy.request({
-        method: 'GET',
-        url: 'http://localhost:3333/providers',
-        headers: {
-            authorization: 'Bearer ' + Cypress.env('apiToken')
-        }
-    }).then(function (response) {
-        expect(response.status).to.eq(200)
-       // cy.log(response.body)
-
-        const providerList = response.body
-
-        providerList.forEach(function (provider) {
-            if (provider.email === providerEmail) {
-                Cypress.env('providerId', provider.id)
-            }
-
-        });
-    })
-})
-
-Cypress.Commands.add('apiLogin', function (user) {
-    const payload = {
-        email: user.email,
-        password: user.password
-    }
-    cy.request({
-        method: 'POST',
-        url: 'http://localhost:3333/sessions',
-        body: payload
-    }).then(function (response) {
-        expect(response.status).to.eq(200)
-        Cypress.env('apiToken', response.body.token)
     })
 })
--- tests/cypress/support/pages/dash/index.js
+++ tests/cypress/support/pages/dash/index.js
@@ -1,5 +1,5 @@
 import header from '../../components/header'
-import {el} from './elements'
+import { el } from './elements'
 
 class DashPage {
 
@@ -7,6 +7,27 @@ class DashPage {
         this.header = header
     }
 
+    calendarShoulBeVisible() {
+
+        cy.get('.DayPicker', { timeout: 10000 })
+            .should('be.visible')
+    }
+
+    selectDay(day) {
+        const target = new RegExp('^' + day + '$', 'g')
+
+        cy.contains('.DayPicker-Day--available', target)
+            .click({ force: true })
+    }
+
+    appointmentShoulBe(customer, hour) {
+
+        cy.contains('div', customer.name, { timeout: 10000 })
+            .should('be.visible')
+            .parent()
+            .contains('span[class=appointment]', hour)
+            .should('be.visible')
+    }
 }
 
 export default new DashPage()
