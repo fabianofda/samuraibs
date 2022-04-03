0e42c13190992626b83bb3a6c0df069928b1263c
Fabiano Dias <fabianofda@gmail.comm>
2022-04-02 21:49:01 -0300

    "trabalhando com factories"

--- tests/cypress/integration/dash.spec.js
+++ tests/cypress/integration/dash.spec.js
@@ -1,45 +1,30 @@
 import loginPage from '../support/pages/login'
 import dashPage from '../support/pages/dash'
+import { customer, provider, appointment } from '../support/factories/dash'
 
 describe('dashboard', function () {
 
     context('quando o cliente faz um agendamento pelo app mobile', function () {
-        const data = {
-            customer: {
-                name: 'Nikki Sixx',
-                email: 'sixx@motleycrue.com',
-                password: 'pwd123',
-                is_provider: false
-            },
-            provider: {
-                name: 'Ramon Valdes',
-                email: 'ramon@televisa.com',
-                password: 'pwd123',
-                is_provider: true
-            },
-            appointmentHour: '17:00'
-        }
 
         before(function () {
 
-            cy.postUser(data.provider)
-            cy.postUser(data.customer)
-            cy.apiLogin(data.customer)
-            // cy.log(Cypress.env('apiToken'))
-            cy.setProviderId(data.provider.email)
-            cy.createAppointment(data.appointmentHour)
+            cy.postUser(provider)
+            cy.postUser(customer)
+            cy.apiLogin(customer)
+            cy.setProviderId(provider.email)
+            cy.createAppointment(appointment.hour)
         })
 
         it('o mesmo deve ser exibido no dashboard', function () {
 
             loginPage.go()
-            loginPage.form(data.provider)
+            loginPage.form(provider)
             loginPage.submit()
 
             dashPage.calendarShoulBeVisible()
             const day = Cypress.env('appointmentDay')
             dashPage.selectDay(day)
-            dashPage.appointmentShoulBe(data.customer, data.appointmentHour)
+            dashPage.appointmentShoulBe(customer, appointment.hour)
 
         })
     })
